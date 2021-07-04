08fd230ec0354c40060f8ab73ac9d9c7d793e47b
Brandon Mitchell <brandon@thewholedoubt.com>
2021-07-04 11:43:02 -0700

    Template-ize Coordinate on its configuration.

--- source/vivaldi/coordinate.d
+++ source/vivaldi/coordinate.d
@@ -1,21 +1,14 @@
 module vivaldi.coordinate;
 
-import vivaldi.config;
-
 import core.time;
 
 debug(vivaldi) {
     import std.experimental.logger : tracef;
 }
 
-// Following "Network Coordinates in the Wild" by Ledlie, et al., use
-// 4 dimensions plus "height".
-// TODO: Template-ize Coordinate on this value.
-private static enum Dimensionality = 4;
-
 private static enum ZeroThreshold = 1.0e-6;
 
-private static enum SecondsToNanos = 1.0e9;
+private static enum NanosPerSecond = 1.0e9;
 
 /**
  * Coordinate represents a point in a Vivaldi network coordinate
@@ -28,50 +21,39 @@ private static enum SecondsToNanos = 1.0e9;
  * the Internet, where link distance typically reflects geographic
  * distance.
  *
- * The non-Euclidean component represents a "height" offset, which
- * represents the distance across an access link to the core
- * Internet. Link distance on access links is typically constrained by
- * bandwidth and congestion.
+ * The non-Euclidean component is a "height" offset, which represents
+ * the distance across an access link to the core Internet. Link
+ * distance on access links is typically constrained by bandwidth and
+ * congestion, rather than geography.
  *
  * See "Vivaldi: A Decentralized Network Coordinate System" by Dabek,
  * et al.
+ *
+ * Params:
+ *      dims = Dimensionality of the coordinate system.
+ *      maxError = Limit to the error any observation may induce.
+ *      minHeight = The minimum height of any Coordinate.
+ *      ce = A tuning factor which impacts the maximum impact an observation
+ *           can have on a Coordinate.
+ *      cc = ditto
+ *      rho = A tuning factor for the affect of gravity exerted by the origin to
+ *            control drift.  See "Network Coordinates in the Wild" by
+ *            Ledlie, et al. for more information.
  */
-struct Coordinate {
-
-    /**
-     * Constructs a new Coordinate at the origin.
-     */
-    this(Config config) pure nothrow @safe @nogc {
-        error = config.maxError;
-        height = config.minHeight;
-    }
-
-    @("defaults")
-    unittest {
-        Config cfg;
-        auto coord = Coordinate(cfg);
-
-        assert(coord.vector == [ 0.0, 0.0, 0.0, 0.0 ]);
-        assert(magnitude(coord.vector) == 0.0);
-        assert(coord.error == cfg.maxError);
-        assert(coord.height == cfg.minHeight);
-    }
-
-    @disable this();
-
-    @("void initialization")
-    unittest {
-        import std.conv : emplace;
-
-        Coordinate coord = void;
-        static assert(!__traits(compiles, emplace(&coord)));
-    }
+struct Coordinate(size_t dims,
+                  double maxError = 1.5,
+                  double minHeight = 1.0e-5,
+                  double ce = 0.25,
+                  double cc = 0.25,
+                  double rho = 150.0)
+     if (dims > 0 && ce < 1.0 && cc < 1.0)
+{
 
     /**
      * Given a round-trip time observation for another node at
      * `other`, updates the estimated position of this Coordinate.
      */
-    void update(const Config* cfg, const Coordinate* other, const Duration rtt)
+    void update(const Coordinate* other, const Duration rtt)
          nothrow @safe @nogc {
 
         import std.math : abs, pow;
@@ -96,12 +78,12 @@ struct Coordinate {
         // error -> large force.
         const double weight = error / total;
 
-        error = err * cfg.ce * weight + error * (1.0 - cfg.ce * weight);
+        error = err * ce * weight + error * (1.0 - ce * weight);
 
-        const double delta = cfg.cc * weight;
+        const double delta = cc * weight;
 
         // NB. force is in seconds
-        double force = delta * (nanos - dist) / SecondsToNanos;
+        double force = delta * (nanos - dist) / NanosPerSecond;
 
         debug(vivaldi) {
             tracef("applying force %f from %s to %s due to RTT %s",
@@ -112,12 +94,12 @@ struct Coordinate {
         }
 
         // Apply the force exerted by the other node.
-        applyForce(cfg, other, force);
+        applyForce(other, force);
 
         // Gravity toward the origin exerts a pulling force which is a
         // small fraction of the expected diameter of the network.
         // "Network Coordinates in the Wild", Sec. 7.2
-        force = -1.0 * pow((magnitude(vector) + height) / cfg.rho, 2.0);
+        force = -1.0 * pow((magnitude(vector) + height) / rho, 2.0);
 
         debug(vivaldi) {
             tracef("applying force %f to %s due to gravity",
@@ -125,65 +107,22 @@ struct Coordinate {
                    this);
         }
 
-        scope auto origin = Coordinate(*cfg);
+        scope Coordinate origin = typeof(this)();
 
         // Apply the force of gravity exerted by the origin.
-        applyForce(cfg, &origin, force);
-    }
-
-    @("update")
-    unittest {
-        import unit_threaded.runner.io;
-        enableDebugOutput();
-        Config cfg;
-        Coordinate c = Coordinate(cfg);
-
-        assert(c.vector == [ 0.0, 0.0, 0.0, 0.0 ]);
-
-        // Place another node above and nearby; update with a high RTT.
-        Coordinate other = Coordinate(cfg);
-        other.vector[2] = 0.001;
-
-        Duration rtt = msecs(200);
-        c.update(&cfg, &other, rtt);
-
-        // The coordinate should be pushed away along the correct axis.
-        assert(c.vector[0] == 0.0);
-        assert(c.vector[1] == 0.0);
-        assert(c.vector[2] < 0.0);
-        assert(c.vector[3] == 0.0);
+        applyForce(&origin, force);
     }
 
     /**
      * Returns the distance to `other` in estimated round-trip time.
      */
     Duration distanceTo(const Coordinate* other) const pure nothrow @safe @nogc {
-        double[Dimensionality] diff = vector[] - other.vector[];
+        double[dims] diff = vector[] - other.vector[];
 
         // NB. height and magnitude are in seconds.
         const double dist = magnitude(diff) + height + other.height;
 
-        return nsecs(cast(long)(dist * SecondsToNanos));
-    }
-
-    @("distanceTo")
-    unittest {
-        Config cfg;
-        cfg.minHeight = 0;
-
-        auto c1 = Coordinate(cfg);
-        c1.vector = [ -0.5, 1.3, 2.4, 0.0 ];
-
-        auto c2 = Coordinate(cfg);
-        c2.vector = [ 1.2, -2.3, 3.4, 0.0 ];
-
-        assert(c1.distanceTo(&c1).total!"msecs" == 0);
-        assert(c1.distanceTo(&c2).total!"msecs" == c2.distanceTo(&c1).total!"msecs");
-        assert(c1.distanceTo(&c2).total!"msecs" == 4104);
-
-        c1.height = 0.7;
-        c2.height = 0.1;
-        assert(c1.distanceTo(&c2).total!"msecs" == 4104 + 800);
+        return nsecs(cast(long)(dist * NanosPerSecond));
     }
 
 private:
@@ -207,20 +146,20 @@ private:
      * represents the distance from the origin in the Internet core in
      * seconds.
      */
-    double[Dimensionality] vector = 0.0;
+    double[dims] vector = 0.0;
 
     /**
      * `height` is the non-Euclidean component of the coordinate,
      * which represents the distance along an access link to the
      * Internet core in seconds.
      */
-    double height;
+    double height = minHeight;
 
     /**
      * `error` is a unitless measure of confidence that this
      * Coordinate represents the true distance from the origin.
      */
-    double error;
+    double error = maxError;
 
     /**
      * Applies a `force` in seconds against this coordinate from the
@@ -230,65 +169,121 @@ private:
      * away from other. If negative, this coordinate will be pulled
      * closer to other.
      */
-    void applyForce(scope const Config* cfg,
-                    scope const Coordinate* other,
+    void applyForce(scope const Coordinate* other,
                     double force) nothrow @safe @nogc {
         import std.algorithm : max;
 
-        double[Dimensionality] unit;
+        double[dims] unit;
         const double mag = unitvector(vector, other.vector, unit);
 
         vector[] += unit[] * force;
 
         if (mag > ZeroThreshold) {
-            height = max((height + other.height) * force / mag + height, cfg.minHeight);
+            height = max((height + other.height) * force / mag + height, minHeight);
         }
     }
 
-    @("applyForce")
-    unittest {
-        version (DigitalMars) {
-            import std.math : isClose;
-        } else version (LDC) {
-            import std.math : approxEqual;
-            alias isClose = approxEqual;
-        }
-
-        Config cfg;
-        cfg.minHeight = 0;
-
-        auto origin = Coordinate(cfg);
-
-        auto above = Coordinate(cfg);
-        above.vector = [ 0.0, 0.0, 2.9, 0.0 ];
-
-        Coordinate c = origin;
-        c.applyForce(&cfg, &above, 5.3);
-        assert(c.vector == [ 0.0, 0.0, -5.3, 0.0 ]);
-
-        auto right = Coordinate(cfg);
-        right.vector = [ 3.4, 0.0, -5.3, 0.0 ];
-        c.applyForce(&cfg, &right, 2.0);
-        assert(c.vector == [ -2.0, 0.0, -5.3, 0.0 ]);
-
-        c = origin;
-        c.applyForce(&cfg, &origin, 1.0);
-        assert(origin.distanceTo(&c) == seconds(1));
-
-        cfg.minHeight = 10.0e-6;
-        origin = Coordinate(cfg);
-        c = origin;
-        c.applyForce(&cfg, &above, 5.3);
-        assert(c.vector == [ 0.0, 0.0, -5.3, 0.0 ]);
-        assert(isClose(c.height, cfg.minHeight + 5.3 * cfg.minHeight / 2.9));
-
-        c = origin;
-        c.applyForce(&cfg, &above, -5.3);
-        assert(c.vector == [ 0.0, 0.0, 5.3, 0.0 ]);
-        assert(isClose(c.height, cfg.minHeight));
-    }
 }
 
+@("defaults")
+unittest {
+    auto coord = new Coordinate!4;
+
+    assert(coord.vector == [ 0.0, 0.0, 0.0, 0.0 ]);
+    assert(magnitude(coord.vector) == 0.0);
+    assert(coord.error == 1.5);
+    assert(coord.height == 1.0e-5);
+}
+
+@("update")
+unittest {
+    auto c = new Coordinate!4;
+
+    assert(c.vector == [ 0.0, 0.0, 0.0, 0.0 ]);
+
+    // Place another node above and nearby; update with a high RTT.
+    auto other = new Coordinate!4;
+    other.vector[2] = 0.001;
+
+    Duration rtt = msecs(200);
+    c.update(other, rtt);
+
+    // The coordinate should be pushed away along the correct axis.
+    assert(c.vector[0] == 0.0);
+    assert(c.vector[1] == 0.0);
+    assert(c.vector[2] < 0.0);
+    assert(c.vector[3] == 0.0);
+}
+
+@("distanceTo")
+unittest {
+    auto c1 = new Coordinate!(4, 1.5, 0);
+    c1.vector = [ -0.5, 1.3, 2.4, 0.0 ];
+
+    auto c2 = new Coordinate!(4, 1.5, 0);
+    c2.vector = [ 1.2, -2.3, 3.4, 0.0 ];
+
+    assert(c1.distanceTo(c1).total!"msecs" == 0);
+    assert(c1.distanceTo(c2).total!"msecs" == c2.distanceTo(c1).total!"msecs");
+    assert(c1.distanceTo(c2).total!"msecs" == 4104);
+
+    c1.height = 0.7;
+    c2.height = 0.1;
+    assert(c1.distanceTo(c2).total!"msecs" == 4104 + 800);
+}
+
+@("applyForce zero height")
+unittest {
+    version (DigitalMars) {
+        import std.math : isClose;
+    } else version (LDC) {
+        import std.math : approxEqual;
+        alias isClose = approxEqual;
+    }
+
+    auto origin = new Coordinate!(4, 1.5, 0);
+
+    auto above = new Coordinate!(4, 1.5, 0);
+    above.vector = [ 0.0, 0.0, 2.9, 0.0 ];
+
+    auto c = *origin;
+    c.applyForce(above, 5.3);
+    assert(c.vector == [ 0.0, 0.0, -5.3, 0.0 ]);
+
+    auto right = new Coordinate!(4, 1.5, 0);
+    right.vector = [ 3.4, 0.0, -5.3, 0.0 ];
+    c.applyForce(right, 2.0);
+    assert(c.vector == [ -2.0, 0.0, -5.3, 0.0 ]);
+
+    c = *origin;
+    c.applyForce(origin, 1.0);
+    assert(origin.distanceTo(&c) == seconds(1));
+}
+
+@("applyForce default height")
+unittest {
+    version (DigitalMars) {
+        import std.math : isClose;
+    } else version (LDC) {
+        import std.math : approxEqual;
+        alias isClose = approxEqual;
+    }
+
+    auto origin = new Coordinate!4;
+    auto c = *origin;
+
+    auto above = new Coordinate!4;
+    above.vector = [ 0.0, 0.0, 2.9, 0.0 ];
+
+    c.applyForce(above, 5.3);
+    assert(c.vector == [ 0.0, 0.0, -5.3, 0.0 ]);
+    assert(isClose(c.height, (1.0e-5 + above.height) * 5.3 / 2.9 + 1.0e-5));
+
+    c = *origin;
+    c.applyForce(above, -5.3);
+    assert(c.vector == [ 0.0, 0.0, 5.3, 0.0 ]);
+    assert(isClose(c.height, 1.0e-5));
+}
 
 // FIXME: scale to prevent overflow
 private double magnitude(size_t D)(const double[D] vec) pure nothrow @safe @nogc
